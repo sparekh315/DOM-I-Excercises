a7c2e14b93cbb27d49af6aa07005395eb67bcb85
sparekh315 <saniaparekh315@gmail.com>
2020-06-16 22:17:09 -0500

    updated html with JSON data

--- js/index.js
+++ js/index.js
@@ -55,6 +55,12 @@ const siteContent = {
 //   }
   const navLinks = document.querySelectorAll('nav a');
   console.log(navLinks);
+  navLinks[0].textContent = siteContent.nav["nav-item-1"];
+  navLinks[1].textContent = siteContent.nav["nav-item-2"];
+  navLinks[2].textContent = siteContent.nav["nav-item-3"];
+  navLinks[3].textContent = siteContent.nav["nav-item-4"];
+  navLinks[4].textContent = siteContent.nav["nav-item-5"];
+  navLinks[5].textContent = siteContent.nav["nav-item-6"];
 
 
   //cta
@@ -65,12 +71,15 @@ const siteContent = {
 //   },
   const ctaHeader = document.querySelector('.cta h1');
   console.log(ctaHeader);
+  ctaHeader.textContent = siteContent.cta.h1;
 
   const ctaButton = document.querySelector('.cta button');
   console.log(ctaButton);
+  ctaButton.textContent = siteContent.cta.button;
 
   const ctaImg = document.querySelector('#cta-img');
   console.log(ctaImg);
+  ctaImg.src = siteContent.cta["img-src"];
 
   //main content
 // },
@@ -90,22 +99,32 @@ const siteContent = {
   // main content top content
   const mainConTopHeaders = document.querySelectorAll('.top-content h4');
   console.log(mainConTopHeaders);
+  mainConTopHeaders[0].textContent = siteContent["main-content"]["features-h4"];
+  mainConTopHeaders[1].textContent = siteContent["main-content"]["about-h4"];
 
   const mainConTopParagraphs = document.querySelectorAll('.top-content p');
   console.log(mainConTopParagraphs);
+  mainConTopParagraphs[0].textContent = siteContent["main-content"]["features-content"];
+  mainConTopParagraphs[1].textContent = siteContent["main-content"]["about-content"];
 
   //main content img
 
   const mainConImg = document.querySelector('#middle-img');
   console.log(mainConImg);
   mainConImg.src = siteContent["main-content"]["middle-img-src"];
+  
+
 
   //main content bottom content
   const mainConBottomHeaders = document.querySelectorAll('.bottom-content h4');
   console.log(mainConBottomHeaders);
+  mainConBottomHeaders[0].textContent = siteContent["main-content"]["services-h4"];
+  mainConBottomHeaders[1].textContent = siteContent["main-content"]["product-h4"];
 
   const mainConBottomParagraphs = document.querySelectorAll('.bottom-content p');
   console.log(mainConBottomParagraphs);
+  mainConBottomParagraphs[0].textContent = siteContent["main-content"]["services-content"];
+  mainConBottomParagraphs[1].textContent = siteContent["main-content"]["product-content"];
 
   //contact
 //   "contact": {
@@ -116,9 +135,14 @@ const siteContent = {
 //   },
   const contactHeader = document.querySelector('.contact h4');
   console.log(contactHeader);
+  contactHeader.textContent = siteContent.contact["contact-h4"];
 
   const contactParagraphs = document.querySelectorAll('.contact p');
   console.log(contactParagraphs);
+  contactParagraphs[0].textContent = siteContent.contact.address;
+  contactParagraphs[1].textContent = siteContent.contact.phone;
+  contactParagraphs[2].textContent = siteContent.contact.email;
+  
 
   //footer
 //   "footer": {
@@ -126,3 +150,4 @@ const siteContent = {
 //   },
   const footerParagraph = document.querySelector('footer p');
   console.log(footerParagraph);
+  footerParagraph.textContent = siteContent.footer.copyright;
